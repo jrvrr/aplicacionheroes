5156e4892f0bcf9431a0ce4b3c46d987b64eecd8
jrvrr <moralesriveraj10@gmail>
2025-03-14 21:19:10 -0600

    feat: login

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
-  standalone: false,
-  
   templateUrl: './register-page.component.html',
-  styles: ``
+  standalone: false
 })
 export class RegisterPageComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellidos: ['', [Validators.required, Validators.minLength(3)]],
+      edad: ['', [Validators.required, Validators.min(18)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onRegister(): void {
+    if (this.registerForm.valid) {
+      const { nombre, apellidos, edad, email, password } = this.registerForm.value;
+      // Aquí iría la lógica para enviar los datos al backend
+      console.log('Formulario enviado', this.registerForm.value);
+    }
+  }
 }
